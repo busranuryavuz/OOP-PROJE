65187af00778f2eae62fee9df85f0f00e7e18129
Büşra Yavuz <48453227+busranuryavuz@users.noreply.github.com>
2020-03-18 20:31:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package otel;
+
+/**
+ * projenin main snıfı proje buradan başlar
+ * 
+ * @author busra
+ * @author ilayda
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        //ilk açılan pencere giriş yap ekranı
+          new otel.gui.GirisYap().setVisible(true);
+    }
+    
+  
+    
+}
